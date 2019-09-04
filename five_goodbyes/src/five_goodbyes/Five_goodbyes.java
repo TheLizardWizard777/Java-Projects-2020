f61d19f081974d071ce8f27129b8d6db10ec42fc
Gabriel Gore <gabriel.gore@tooeleschools.org>
2019-09-04 14:16:33 -0600

    projects 9/4

---
+++ five_goodbyes/src/five_goodbyes/Five_goodbyes.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package five_goodbyes;
+
+/**
+ * @author gabriel.gore
+ *
+ */
+public class Five_goodbyes {
+
+	/**
+	 * 
+	 */
+	public Five_goodbyes() {
+		System.out.println("Time to start i guess...");
+		System.out.println("1. Antio sas");
+		System.out.println("2. Zàijiàn");
+		System.out.println("3. Aloha");
+		System.out.println("4. Bayartai");
+		System.out.println("5. Proshchay");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Five_goodbyes();
+
+	}
+
+}
---
+++ hello_world_2.0/src/gggore_jello_world/Hello_world.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package gggore_jello_world;
+
+/**
+ * @author gabriel.gore
+ * Hello World
+ * 9/4/19
+ */
+public class Hello_world {
+
+	/**
+	 * 
+	 */
+	public Hello_world() {
+		System.out.println("Hello World");
+		System.out.println("yooo I can put whatever i want here yooooo");
+		String hello = "Hello ;)";
+		System.out.println(hello);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Hello_world();
+
+	}
+
+}
